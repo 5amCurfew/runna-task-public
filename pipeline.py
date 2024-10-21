23907f979f65aa091b0dc56d053edc8525b3833d
5amCurfew <samueltobyknight@gmail.com>
2024-10-21 18:34:42 +0100

    all transformations

--- pipeline.py
+++ pipeline.py
@@ -15,9 +15,9 @@ FAILURE_TAG = "FAILURE"
 
 transformations = [
     "fct__activities",
-    #'dim__plans',
-    #'dim__workouts',
-    #'bdg__activity_to_laps',
+    'dim__plans',
+    'dim__workouts',
+    'bdg__activity_to_laps',
     "bdg__workout_to_steps",
 ]
 
